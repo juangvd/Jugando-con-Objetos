0afbeb79e5489ffc5b6719437dfd2d662de2b62b
juangvd <77943731+juangvd@users.noreply.github.com>
2021-05-04 06:00:22 -0400

    Add files via upload

---
+++ Jugando-con-Objetos-juan-vega.js
@@ -0,0 +1,61 @@
+//                            JUgando con Objetos                          //
+
+/////////////////////////////////////////////////////////////////////////////
+
+
+//  Practica iterar por medio de un array de objetos/diccionarios. Imagina que se te entrega un array de objetos. Por ejemplo,
+/*
+
+
+//  1.- ¿Cómo harías print/log de la edad de John?
+
+
+var users = [{ name: "Michael", age: 37 }, { name: "John", age: 30 }, { name: "David", age: 27 }];
+
+for (var i = 0; i < users.length; i++) {
+    if (users[i].name == "John") {
+        console.log(users[i].age);
+    }
+}
+
+
+//  2.- ¿Cómo harías print/log del nombre del primer objeto?
+
+
+var users = [{ name: "Michael", age: 37 }, { name: "John", age: 30 }, { name: "David", age: 27 }];
+
+for (var i = 0; i < users.length; i++) {
+    if (users[i].name == "Michael") {
+        console.log(users[i].name);
+    }
+}
+
+
+//  3.- ¿Cómo harías print/log del nombre y la edad de cada usuario utilizando un for loop? Tu output debería verse algo como esto
+
+
+var users = [{ name: "Michael", age: 37 }, { name: "John", age: 30 }, { name: "David", age: 27 }];
+
+for (var i = 0; i < users.length; i++) {
+    console.log(users[i].name + '-' + users[i].age);
+}
+
+
+//  4.- ¿Cómo harías para imprimir el nombre de los mayores de edad?
+
+
+//   Michael - 37
+//   John - 30
+//   David - 27
+
+
+var users = [{ name: "Michael", age: 37 }, { name: "John", age: 30 }, { name: "David", age: 27 }];
+
+for (var i = 0; i < users.length; i++) {
+    if (users[i].age >= 18) {
+        console.log(users[i].name);
+    }
+}
+
+
+*/
